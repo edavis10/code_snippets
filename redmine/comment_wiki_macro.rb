32720ac72c660d893bb1c52823f636ca4ac53405
Eric Davis <edavis@littlestreamsoftware.com>
2010-04-22 09:40:28 -0700

    Prototype comment macro

---
+++ redmine/comment_wiki_macro.rb
@@ -0,0 +1,19 @@
+#
+# vendor/plugins/redmine_comment_wiki_macro/init.rb
+#
+require 'redmine'
+
+Redmine::Plugin.register :redmine_gist do
+  name 'Redmine YouTube plugin'
+  author 'Eric Davis'
+  description 'Allows commenting out a section of the Redmine wiki'
+  version '0.0.1'
+
+  Redmine::WikiFormatting::Macros.register do
+    desc "Comment out a section"
+    macro :comment do |obj, args|
+      return '' # Return nothing, but the content is saved
+    end
+
+  end
+end
